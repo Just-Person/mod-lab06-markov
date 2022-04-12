17d65a206f8f5a4333f8fd0c5f420d6f74a17d64
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-04-12 17:26:21 +0300

    Create textgen.h

---
+++ include/textgen.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <map>
+class textgen
+{
+private:
+    int index;
+    int prefixlength;
+    int resultlength;
+    std::string path;
+    std::string result;
+public:
+    std::vector < std::vector < std::string>> table;
+    textgen(std::string path)
+    {
+        this->path = path;
+        index = -1;
+        prefixlength = 1;
+        result = "";
+        table = std::vector<std::vector <std::string>>();
+    }
+    void setprefixlength(int);
+    int getprefixlength();
+    std::string getall();
+    std::vector<int> getindex();
+    void generate();
+    std::string getprefix();
+    std::string getsuffix();
+    void randomizeindex();
+    void getnewindex(std::vector<std::string>);
+    void readfile();
+    void checkprefix(std::vector<std::string>);
+    int getresultlength();
+    std::string getresult();
+    std::string getsuffix(int);
+    std::string getsuffix(int, int);
+};
