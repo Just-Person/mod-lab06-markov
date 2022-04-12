7c9eafa97a60e2e8ffc8887d5f9bc42fb79cd2d6
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-04-12 21:40:16 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,109 +5,57 @@
 #include <ctime>
 TEST(TEST1, TrueOrFalse) {
   setlocale(LC_ALL, "rus");
+  textgen text = textgen("Test1.txt");
+  srand(time(NULL));
+  text.setprefixlength(1);
+  text.readfile();
+  EXPECT_EQ("Всем ; привет ; меня ; зовут ; Илья ; и ; это ; первый ; тест ; по ; этой ; лабораторной ; ", text.getprefix());
+}
+
+TEST(TEST2, TrueOrFalse) {
+  setlocale(LC_ALL, "rus");
+  textgen text = textgen("Test1.txt");
+  srand(time(NULL));
+  text.setprefixlength(3);
+  text.readfile();
+  EXPECT_EQ("Всем привет меня ; привет меня зовут ; меня зовут Илья ; зовут Илья и ; Илья и это ; и это первый ; это первый тест ; первый тест по ; тест по этой ; по этой лабораторной ; этой лабораторной Всем ; лабораторной Всем привет ; ", text.getprefix());
+}
+
+TEST(TEST3, TrueOrFalse) {
+  setlocale(LC_ALL, "rus");
+  textgen text = textgen("Test2.txt");
+  srand(time(NULL));
+  text.setprefixlength(1);
+  text.readfile();
+  EXPECT_EQ("Всем - привет ; привет - этот ; этот - текст ; текст - создан ; создан - для ; для - первого второго третьего четвертого ; первого - теста ; теста - для для для Всем ; второго - теста ; третьего - теста ; четвертого - теста ; ", text.getall());
+}
+
+TEST(TEST4, TrueOrFalse) {
+    	setlocale(LC_ALL, "rus");
 	textgen text = textgen("Test1.txt");
 	srand(time(NULL));
 	text.setprefixlength(1);
 	text.readfile();
-	std::cout << text.getall();
-	for (int i = 0; i < 300; i++)
-	{
-		text.generate();
-	}
-	std::cout << text.getresultlength();
-    EXPECT_EQ(States::WAIT, automata.getState());
-}
-
-TEST(TEST2, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.check();
-    EXPECT_EQ(States::WAIT, automata.getState());
-}
-
-TEST(TEST3, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.coin(10);
-    automata.coin(10);
-    automata.coin(10);
-    EXPECT_EQ(States::ACCEPT, automata.getState());
-}
-
-TEST(TEST4, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.coin(10);
-    automata.coin(10);
-    automata.coin(10);
-    automata.choiceuser(1);
-    EXPECT_EQ(States::CHOSEN, automata.getState());
+    EXPECT_EQ("зовут", text.getsuffix(3));
 }
 
 
 TEST(TEST5, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.coin(10);
-    automata.coin(10);
-    automata.coin(10);
-    automata.choiceuser(1);
-    automata.check();
-    EXPECT_EQ(States::WAIT, automata.getState());
+setlocale(LC_ALL, "rus");
+textgen text = textgen("Test2.txt");
+srand(time(NULL));
+text.setprefixlength(1);
+text.readfile();
+std::cout << text.getsuffix(6, 3);
+EXPECT_EQ("третьего", text.getsuffix(6, 3));
 }
 
 TEST(TEST6, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.coin(100);
-    automata.choiceuser(1);
-    automata.check();
-    EXPECT_EQ(States::MAKING, automata.getState());
-}
-
-TEST(TEST7, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.coin(100);
-    automata.choiceuser(1);
-    automata.check();
-    automata.cook();
-    EXPECT_EQ(States::READY, automata.getState());
-}
-
-TEST(TEST8, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.coin(100);
-    automata.choiceuser(1);
-    automata.check();
-    automata.cook();
-    EXPECT_EQ(40, automata.getCash());
-}
-
-TEST(TEST9, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.coin(60);
-    automata.choiceuser(1000);
-    EXPECT_EQ(States::WAIT, automata.getState());
-}
-
-TEST(TEST10, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.cook();
-    EXPECT_EQ(States::WAIT, automata.getState());
-}
-
-TEST(TEST11, TrueOrFalse) {
-    Automata automata;
-    automata.on();
-    automata.coin(60);
-    automata.choiceuser(1);
-    automata.check();
-    automata.cook();
-    automata.finish();
-    automata.off();
-    EXPECT_EQ(States::OFF, automata.getState());
+    setlocale(LC_ALL, "rus");
+	textgen text = textgen("Test3.txt");
+	srand(time(NULL));
+	text.setprefixlength(5);
+	text.readfile();
+	text.generate(100);
+    EXPECT_EQ(100, text.getresultlength());
 }
