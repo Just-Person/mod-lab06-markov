c4bf63fc6dd8e274a49d716019ef6d5a3bf6ddd5
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-04-12 17:25:21 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "textgen.h"
+#include <locale>
+#include <ctime>
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    textgen text = textgen("test.txt");
+    srand(time(NULL));
+    text.setprefixlength(1);
+    text.readfile();
+    std::cout << text.getall();
+    for (int i = 0; i < 300; i++)
+    {
+        text.generate();
+    }
+    std::cout << text.getresultlength();
+
+}
