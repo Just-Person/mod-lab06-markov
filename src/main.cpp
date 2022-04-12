e8aaf4586bd9158b85ab559415dae0bdeebf4b0b
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-04-12 20:58:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include <iostream>
-#include "textgen.h"
 #include <locale>
 #include <ctime>
-int main()
-{
-    setlocale(LC_ALL, "rus");
-    textgen text = textgen("test.txt");
-    srand(time(NULL));
-    text.setprefixlength(1);
-    text.readfile();
-    std::cout << text.getall();
-    for (int i = 0; i < 300; i++)
-    {
-        text.generate();
-    }
-    std::cout << text.getresultlength();
+#include "textgen.h"
+int main() {
+setlocale(LC_ALL, "rus");
+return 1;
 }
