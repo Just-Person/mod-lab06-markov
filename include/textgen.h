8b86c64670ee002bab753610d82d2fe59efec93f
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-04-12 17:53:21 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,40 +1,39 @@
 // Copyright 2022 UNN-IASR
 #pragma once
-#include <iostream>
 #include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <map>
-class textgen
-{
-private:
-    int index;
-    int prefixlength;
-    int resultlength;
-    std::string path;
-    std::string result;
-public:
-    std::vector < std::vector < std::string>> table;
-    textgen(std::string path)
-    {
-        this->path = path;
-        index = -1;
-        prefixlength = 1;
-        result = "";
-        table = std::vector<std::vector <std::string>>();
-    }
-    void setprefixlength(int);
-    int getprefixlength();
-    std::string getall();
-    std::vector<int> getindex();
-    void generate();
-    std::string getprefix();
-    std::string getsuffix();
-    void randomizeindex();
-    void getnewindex(std::vector<std::string>);
-    void readfile();
-    void checkprefix(std::vector<std::string>);
-    int getresultlength();
-    std::string getresult();
-    std::string getsuffix(int);
-    std::string getsuffix(int, int);
+
+class textgen {
+ private:
+int index;
+int prefixlength;
+int resultlength;
+std::string path;
+std::string result;
+ public:
+std::vector < std::vector < std::string>> table;
+textgen(std::string path) {
+this->path = path;
+index = -1;
+prefixlength = 1;
+result = "";
+table = std::vector<std::vector <std::string>>();
+}
+void setprefixlength(int);
+int getprefixlength();
+std::string getall();
+std::vector<int> getindex();
+void generate();
+std::string getprefix();
+std::string getsuffix();
+void randomizeindex();
+void getnewindex(std::vector<std::string>);
+void readfile();
+void checkprefix(std::vector<std::string>);
+int getresultlength();
+std::string getresult();
+std::string getsuffix(int);
+std::string getsuffix(int, int);
 };
