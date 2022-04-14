cdab189aeb75c75d9ff6e1f81a9a8122d6a5352f
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-04-14 08:10:28 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,16 +1,11 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
 #include <locale.h>
-#include <io.h>
-#include <fcntl.h>
 #include <ctime>
 #include <iostream>
 #include "textgen.h"
 TEST(TEST1, TrueOrFalse) {
 textgen text = textgen("Test1.txt");
-_setmode(_fileno(stdout), _O_U16TEXT);
-_setmode(_fileno(stdin),  _O_U16TEXT);
-_setmode(_fileno(stderr), _O_U16TEXT);
 srand(time(NULL));
 text.setprefixlength(1);
 text.readfile();
